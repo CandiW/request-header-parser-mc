da4795257d84b6d4eec2d980cf3b3551190904d1
CandiW <candisw06@gmail.com>
2017-05-18 08:35:39 -0700

    update index.js

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ let app = express();
 app.get('/',function(request,response){
     let header = request.headers;
     let object = {
-        ipaddress: request.ip['X-Forwarded-For'] || request.connection.remoteAddress,
+        ipaddress: header['x-forwarded-for'] || request.connection.remoteAddress,
         language: header['accept-language'].split(',')[0],
-        software: header['user-agent'].split('(')[0]
+        software: header['user-agent'].split('(')[1]
     }
     response.send(object);
-    repsonse.send(header);
 });
 
 app.listen(process.env.PORT || 3000);
