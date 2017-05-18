0a181eb622f08b6bb19c74d319a4e85570ab6235
CandiW <candisw06@gmail.com>
2017-05-18 08:14:07 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express');
 let app = express();
 
-
-function getHeader(header){
-    console.log(header);
-    return JSON.stringify(header);
-}
-
-app.get('/whoami',function(request,response){
-    let header = getHeader(request.headers);
+app.get('/',function(request,response){
+    let header = request.headers;
     let object = {
         ipaddress: request.ip,
-        language: header[accept-language],
-        software: header[user-agent]
+        language: header['accept-language'],
+        software: header['user-agent']
     }
     response.send(object);
 });
